855011b7b979c4fb1fdaaccc11f5b5677cb799cf
Sebastian Bengtegård <sebastianbengtegard@gmail.com>
2021-03-23 23:44:23 +0100

    Import game creation as well

--- events/index.js
+++ events/index.js
@@ -1,9 +1,11 @@
 // This is where we `require` all of our event handlers ('event:name': handler)
 const debug = require('./debug');
 const connection = require('./connection');
+const gameCreation = require('./game-creation.js');
 
 // And then combine them into one object
 module.exports = {
   debug,
   connection,
+  gameCreation,
 };
