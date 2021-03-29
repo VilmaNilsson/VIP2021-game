20a3ae8b3c9573b83435e5cb6c2a9bb9aff01333
Jasmine Jasse <jasminejasses@gmaill.com>
2021-03-29 16:35:48 +0200

    First version: spell - unlock player
    Co-Authored-By: kojala <75684413+kajsaojala@users.noreply.github.com>

--- spells/index.js
+++ spells/index.js
@@ -1,7 +1,9 @@
 // This is where we `require` all of our spell handlers ('event:name': handler)
 const lockSpells = require('./locks.js');
+const unlockPlayer = require('./unlock_player.js');
 
 // And then combine them into one object
 module.exports = {
   lockSpells,
+  unlockPlayer,
 };
---
+++ spells/unlock_player.js
@@ -0,0 +1,35 @@
+// Unlocks a player that is targeted with a "locked spell" fx. annoy
+
+// Takes two params, context = object containing functions to operate on state, payload = data sent from client-side ex username or player-id for the targeted player
+function unlockPlayer(context, payload){
+
+    // First we'll get the game state
+    const game = context.getGameState();
+
+    // In this spell we'll receive the player id (from frontend) (the player to be unlocked)
+    const playerId = payload.player;
+    // We'll extract the player from state.game
+    const player = game.players[playerId];
+
+    // They tried targeting an unknown player
+    if (player === undefined) {
+        context.send('spell:unlock:player:fail', { errorCode: 2 });
+        return;
+    }
+
+    // We always use the `properties` key for changing values
+    // We want to unlock the player
+    player.properties.locked = false;
+
+    // Overwrite the player once it has been modified
+    game.players[playerId] = player;
+
+    // Save our changes, game is an object we want to merge in to gameState
+    context.updateGameState(game);
+    // Do we want to broadcast it to all players? 
+    //context.broadcastToGame('player:unlocked', { player: playerId });
+};
+
+module.exports = {
+    'spell:unlock:player': unlockPlayer,
+};
