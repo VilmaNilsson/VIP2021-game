bf2b3333c2a5de9566ec5e4ced917fe1065499f8
Lucas G. Alonso <56822984+lgalonso@users.noreply.github.com>
2021-03-29 15:12:25 +0200

    replaced files for event handler

--- events/teamLeave.js
+++ events/teamLeave.js
@@ -1,10 +1,13 @@
 function teamLeave(context, payload) {
+    // Init variables
+    const playerState = context.getPlayerState();
+    const { playerId } = playerState.id;
+
     // TO-DO: update player, -1 for no team
 
     // TO-DO: update game state
 
     // TO-DO: broadcast event 
-
 }
 
 export default {
--- src/events/index.js
+++ src/events/index.js
@@ -1,9 +1,7 @@
 import connection from './connection';
 import playerLogin from './playerLogin'
-import teamLeave from './teamLeave'
 
 export default {
   connection,
   playerLogin,
-  teamLeave,
 };
