8f7672b57c5201d80046e0edfe1c04567164cbfa
LudvigStridsberg <77850704+LudvigStridsberg@users.noreply.github.com>
2021-03-29 12:39:01 +0200

    First version of the Half Station Salary-spell
    Added a file containing the spell and "required" it in the spell-index-file. The spell-file copies a lot of code from the Lock Station-spell.

--- spells/index.js
+++ spells/index.js
@@ -1,7 +1,9 @@
 // This is where we `require` all of our spell handlers ('event:name': handler)
 const lockSpells = require('./locks.js');
+const halfSalaryStation = require('./half_salary_station');
 
 // And then combine them into one object
 module.exports = {
   lockSpells,
+  halfSalaryStation,
 };
