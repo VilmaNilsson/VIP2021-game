5a3769c62357bc7d63bea6e81cee7d973193a142
LudvigStridsberg <77850704+LudvigStridsberg@users.noreply.github.com>
2021-03-29 16:14:14 +0200

    First version of the Double Station Salary-spell and fix of Half Station Salary-file
    Added a file containing the spell and "required" it in the spell-index-file. The spell-file copies a lot of code from the Half station salary-spell.
    
    I also changed the Half Station Salary-file so that it exports the right function (It didn't before).

--- spells/double_salary_station.js
+++ spells/double_salary_station.js
@@ -1,38 +1,38 @@
-function halfSalaryStationSpell(context, payload) {
+function doubleSalaryStationSpell(context, payload) {
     // First we'll get the game state
     const game = context.getGameState();
   
     // We're not in a game
     if (game === null) {
-      context.send('spell:station:half-salary:fail', { errorCode: 0 });
+      context.send('spell:station:double-salary:fail', { errorCode: 0 });
       return;
     }
   
     // We're not in the play phase
     if (game.properties.phase.type !== 2) {
-      context.send('spell:station:half-salary:fail', { errorCode: 1 });
+      context.send('spell:station:double-salary:fail', { errorCode: 1 });
       return;
     }
   
-    // In this spell we'll receive the station index (the one to get it's salary cut)
+    // In this spell we'll receive the station index (the one to get it's salary increased)
     const stationIndex = payload.station;
     // We'll extract the station
     const station = game.stations[stationIndex];
   
     // They tried targeting an unknown station
     if (station === undefined) {
-      context.send('spell:station:half-salary:fail', { errorCode: 2 });
+      context.send('spell:station:double-salary:fail', { errorCode: 2 });
       return;
     }
   
     // We always use the `properties` key for changing values
-    station.properties.salaryMultiplier = .5;
+    station.properties.salaryMultiplier = 2;
     // Then we'll insert the station once it has been modified
     game.stations[stationIndex] = station;
     // Save our changes
     context.updateGameState(game);
     // And broadcast it to all players
-    context.broadcastToGame('station:half-salary', { station: stationIndex });
+    context.broadcastToGame('station:double-salary', { station: stationIndex });
 
     // Reset the station's salary on the next salary payout (ie. Tick)
     context.onNextGameTick(() => {
@@ -47,5 +47,5 @@ function halfSalaryStationSpell(context, payload) {
 }
 
 module.exports = {
-  'spell:station:half-salary': halfSalaryStationSpell,
-};
+    'spell:station:double-salary': doubleSalaryStationSpell,
+  };
