ab7436c3ee5374c3a6a55d3b10ea258b11310824
Louise Edström <louise.edstrom98@gmail.com>
2021-03-31 09:58:21 +0200

    Last fixings
    im sorry ;_;

--- events/teamJoin.js
+++ events/teamJoin.js
@@ -1,16 +1,16 @@
 function teamJoin(context, payload) {
   const playerState = context.getPlayerState();
-  const playerId = context.id;
+  const playerId = context.id();
   const game = context.getGameState();
   const player = game.players[playerId];
   const teamIndex = payload.team;
-  const teamName = game.teams[payload];
+  const teamName = game.teams[teamIndex].name;
 
   playerState.team = teamIndex;
   context.updatePlayerState(playerState);
   player.team = teamIndex;
   context.updateGameState(game);
-  context.broadcast('team:joined', { playerId, teamName });
+  context.broadcastToGame('team:joined', { playerId, teamName });
 }
 
 module.exports = {
