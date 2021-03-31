98673c5d0c308b3b3da6998abfbaad25960315d7
LudvigStridsberg <77850704+LudvigStridsberg@users.noreply.github.com>
2021-03-31 11:52:50 +0200

    First version of the getCurrTeamScores-function
    Added the function into calculator.js.

--- calculator.js
+++ calculator.js
@@ -104,7 +104,25 @@ function getTeamSalaries(game) {
   return SCORE_COUNTER;
 }
 
+function getCurrTeamScores(game){
+  // Unpack the teams from the recieved gamestate
+  const {teams} = game;
+
+  // Create a empty object
+  let currTeamScores = {};
+
+  // Loop through all teams and add a property to the object for each team which
+  // has the team's index as key and their score as value
+  for (let i = 0; i < teams.length; i++) {
+    currTeamScores[i] = teams[i].properties.score;
+  }
+
+  // Return the now filled object
+  return currTeamScores;
+}
+
 module.exports = {
   getTickDuration,
   getTeamSalaries,
+  getCurrTeamScores,
 };
