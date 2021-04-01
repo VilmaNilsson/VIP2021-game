06a8005a519e3f3767cffefc0a0698a3d4e77016
Louise Edström <louise.edstrom98@gmail.com>
2021-04-01 14:01:37 +0200

    Fixed camel case
    Co-Authored-By: Lucas G. Alonso <56822984+lgalonso@users.noreply.github.com>

--- utils.js
+++ utils.js
@@ -130,14 +130,14 @@ function createStation(state = {}) {
 
 // Create racks for a station based on the number of teams and tokens
 function createRacks(nrOfTeams, nrOfTokens) {
-  const random_rack = Array.from({ length: nrOfTokens }).map(() => {
+  const randomRack = Array.from({ length: nrOfTokens }).map(() => {
     return {
       token: Math.floor(Math.random() * nrOfTokens),
     };
   });
   return Array.from({ length: nrOfTeams }).map(() => {
     return {
-      slots: random_rack,
+      slots: randomRack,
     };
   });
 }
