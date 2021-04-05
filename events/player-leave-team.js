f36cb637f4665d33874e647c630cb634a3be0d61
Sebastian Bengtegård <sebastianbengtegard@gmail.com>
2021-04-05 22:35:38 +0200

    minor refactor

--- events/player-leave-team.js
+++ events/player-leave-team.js
@@ -1,16 +1,16 @@
 function teamLeave(context) {
   // Init variables
   const playerId = context.id();
-  const gameState = context.getGameState();
+  const game = context.getGameState();
   const player = gameState.players[playerId];
-  const { team } = gameState.players[playerId];
+  const { team } = player;
 
   // Update player, -1 for no team
   player.team = -1;
 
   // Update game state
-  gameState.players[playerId] = player;
-  context.updateGameState(gameState);
+  game.players[playerId] = player;
+  context.updateGameState(game);
 
   // Broadcast event
   context.broadcastToGame('team:left', { playerId, team });
