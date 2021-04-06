1d9e27d4ae542fa7ed58ab5d0ef672ae95a70d97
Sebastian Bengtegård <sebastianbengtegard@gmail.com>
2021-04-06 15:33:12 +0200

    reverted my changes

--- calculator.js
+++ calculator.js
@@ -44,8 +44,7 @@ function getTeamSalaries(game) {
   for (let i = 0; i < stations.length; i += 1) {
     for (let j = 0; j < stations[i].racks.length; j += 1) {
       stations[i].racks[j].slots.forEach((token) => {
-        // NOTE: fails if the slot is empty (null)
-        if (token !== null && token.token >= 0) {
+        if (token.token >= 0) {
           STATIONS_PER_TEAM[j][i][token.token] += 1;
         }
       });
