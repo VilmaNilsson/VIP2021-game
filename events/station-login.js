2a6fccf24fe6e95c1cc301a57986aa34b473e065
Sebastian Bengtegård <sebastianbengtegard@gmail.com>
2021-04-06 15:33:30 +0200

    fixed station login

--- events/station-login.js
+++ events/station-login.js
@@ -3,7 +3,7 @@ const calc = require('../calculator.js');
 
 function stationLogin(context, payload) {
   // Unload the payload into a constant
-  const { station } = payload;
+  const stationIndex = payload.station;
 
   // Get the gamestate
   const game = context.getGameState();
@@ -12,7 +12,7 @@ function stationLogin(context, payload) {
   const playerId = context.id();
 
   // Get the station- and playerobject so that we can get their attributes
-  const stationObj = game.stations[station];
+  const station = game.stations[stationIndex];
   const player = game.players[playerId];
 
   // We're not in a game
@@ -20,44 +20,58 @@ function stationLogin(context, payload) {
     context.send('event:station:login:fail', { errorCode: 0 });
     return;
   }
+
   // We're not in the play phase
   if (game.properties.phase.type !== 2) {
     context.send('event:station:login:fail', { errorCode: 1 });
     return;
   }
+
   // If the requested station didn't exist
-  if (stationObj === undefined) {
+  if (station === undefined) {
     context.send('event:station:login:fail', { errorCode: 2 });
     return;
   }
-  // If the station is locked or the player is unable to do anything they shouldn't be able
-  // to log into the station
-  if (stationObj.properties.locked === true
-      || player.properties.locked === true) {
+
+  // If the station is locked or the player is unable to do anything they
+  // shouldn't be able to log into the station
+  if (station.properties.locked === true || player.properties.locked === true) {
     context.send('event:station:login:fail', { errorCode: 3 });
     return;
   }
 
   // Calculate the total login-time by calling a function in calculator.js
-  const loginTime = calc.getLoginTime(game, playerId, station);
+  const loginTime = calc.getLoginTime(game, playerId, stationIndex);
 
   // Create a timestamp for the start of the login
   const start = Date.now();
 
   // Change the player's inStation-property
-  game.players[playerId].properties.inStation = { station, start, loginTime };
+  game.players[playerId].properties.inStation = {
+    station: stationIndex,
+    start,
+    loginTime,
+  };
 
   // Update the gamestate (which includes the player)
   context.updateGameState(game);
 
+  // TODO: cancel when logging into more
+  // TODO: people logging in should be able to see everything live
   // Send the login-wait-event to the user so that they start waiting
-  context.send('station:login:wait', { station, loginTime });
+  const { racks } = station;
+  context.send('station:login:wait', {
+    station: stationIndex,
+    racks,
+    start,
+    loginTime,
+  });
 
   // After the defined time the user is allowed into the station
   setTimeout(() => {
     const game = context.getGameState();
-    const { racks } = game.stations[station];
-    context.send('station:login:done', { station, racks });
+    const { racks } = game.stations[stationIndex];
+    context.send('station:login:done', { station: stationIndex, racks });
   }, loginTime * 1000);
 }
 
