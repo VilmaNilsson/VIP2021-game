c16782fb34dd89d19c2b261b1d1182b3934af136
Sebastian Bengtegård <sebastianbengtegard@gmail.com>
2021-04-11 12:41:56 +0200

    refactor

--- events/player-join-team.js
+++ events/player-join-team.js
@@ -1,6 +1,7 @@
 function teamJoin(context, payload) {
   const playerId = context.id();
   const game = context.getGameState();
+  const player = game.players[playerId];
 
   const teamIndex = payload.team;
   const team = game.teams[teamIndex];
@@ -11,11 +12,12 @@ function teamJoin(context, payload) {
   }
 
   // Update the player within the game
-  game.players[playerId].team = teamIndex;
+  player.team = teamIndex;
+  game.players[playerId] = player
   context.updateGameState(game);
 
   context.broadcastToGame('team:joined', { playerId, team: teamIndex });
-  context.send('team:yours', { team });
+  context.send('team:yours', { team: teamIndex });
 }
 
 module.exports = {
