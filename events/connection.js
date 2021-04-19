f5c5881573b2dd7f0996d1a5891df8e41d65894f
Sebastian Bengtegård <sebastianbengtegard@gmail.com>
2021-04-19 13:36:31 +0200

    Fixed issue with player disconnects

--- events/connection.js
+++ events/connection.js
@@ -5,18 +5,23 @@ function playerConnect() {
 // Whenever a player disconnects
 function playerDisconnect(context) {
   const player = context.getPlayerState();
-  const properties = Object.keys(player);
 
   // We'll remove a connected player if they only have 2 properties or less,
   // which means if they only connected and never logged in we'll remove them if
   // they disconnect
-  if (properties.length <= 2) {
+  if (player === null) {
     context.removePlayer(player.id);
-  } else {
-    // Otherwise we'll set their online status to false
-    context.updatePlayerState({ online: false });
-    context.broadcastToGame('player:disconnect', { id: context.id() });
+    return;
   }
+
+  if (properties.length <= 2) {
+    const properties = Object.keys(player);
+    return;
+  }
+
+  // Otherwise we'll set their online status to false
+  context.updatePlayerState({ online: false });
+  context.broadcastToGame('player:disconnect', { id: context.id() });
 }
 
 function playerReconnect() {
