6e1a429857010216caca7599a0c7e6d66bef6509
Sebastian Bengtegård <sebastianbengtegard@gmail.com>
2021-04-23 16:42:31 +0200

    added failing and fixed typos

--- spells/player-temporary-pocket.js
+++ spells/player-temporary-pocket.js
@@ -1,24 +1,33 @@
 function activateTemporaryPocket(context) {
+  const start = Date.now();
   const duration = 120 * 1000;
   const playerId = context.id();
   const game = context.getGameState();
   const player = game.players[playerId];
 
-  player.properties.temporaryPockedLocked = false;
+  // Already unlocked
+  if (player.properties.temporaryPocketLocked === false) {
+    context.send('player:temporary-pocket:fail', { errorCode: 1 });
+    return;
+  }
+
+  player.properties.temporaryPocketLocked = false;
   game.players[playerId] = player;
   context.updateGameState(game);
 
-  context.send('player:temporary-pocket', { duration });
+  context.send('player:temporary-pocket', { start, duration });
 
   context.setTimeout(() => {
     const playerId = context.id();
     const game = context.getGameState();
     const player = game.players[playerId];
-    player.properties.temporaryPockedLocked = true;
+    player.properties.temporaryPocketLocked = true;
     game.players[playerId] = player;
     context.updateGameState(game);
     context.send('player:temporary-pocket:faded', {});
   }, duration);
+
+  return true;
 }
 
 module.exports = {
