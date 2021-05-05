27ac24f17f24bc78c0087ab17a0d5f36884f1146
Jasmine Jasse <jasminejasses@gmaill.com>
2021-05-05 10:32:58 +0200

    more stuff added

--- src/views/plan.js
+++ src/views/plan.js
@@ -1,5 +1,3 @@
-console.log('hej')
-
 import {
   PlanTimer,
   PlanActions,
@@ -12,21 +10,38 @@ function PlanView(context) {
 
   el.innerHTML = `
     <div id='controlPanel'>
-      <div id='rocketBox'><div class='rocket'>Rocket</div><div class='rocket'>Rocket</div><div class='rocket'>Rocket</div><div class='rocket'>Rocket</div></div>
+      <div id='rocketBox'><div class='rocket one'>000</div><div class='rocket two'>000</div><div class='rocket three'>000</div><div class='rocket four'>000</div></div>
       <div id="timer"></div>
     </div>
-    <div id="actions"></div>
+    <div id="actions">
+      <div class='availableAction'><div class='actionName'>Secret</div><br><div class='actionCooldown'>Cooldown 30s</div><br><div class='actionInfo'>Blablalbblbla</div></div>
+      <div class='availableAction'> - </div>
+      <div class='availableAction'> - </div>
+      <div class='availableAction'> - </div>
+      <div class='availableAction'> - </div>
+      <div class='availableAction'> - </div>
+      <div class='availableAction'> - </div>
+      <div class='availableAction'> - </div>
+    </div>
     <div id="player-actions">
-      <div class='chosenAction'>chosen Action</div>
-      <div class='chosenAction'>chosen Action</div>
-      <div class='chosenAction'>chosen Action</div>
-      <div class='chosenAction'>chosen Action</div>
+      <div class='chosenAction'> - </div>
+      <div class='chosenAction'> - </div>
+      <div class='chosenAction'> - </div>
+      <div class='chosenAction'> - </div>
     </div>
   `;
 
   const timerEl = el.querySelector('#timer');
   const actionsEl = el.querySelector('#actions');
   const playerActionsEl = el.querySelector('#player-actions');
+  const availableAction = el.querySelectorAll('.availableAction');
+  console.log(availableAction);
+  availableAction.forEach(function(thisAction){
+    console.log(thisAction)
+    thisAction.addEventListener('click', function(){
+      console.log(this.childNodes[0].innerHTML)
+    })
+  })
 
   // NOTE: we should probably create a `PlanTimer` if their looks differ
   PlanTimer(timerEl, context);
