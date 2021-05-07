fe8213b55fc66230d354d21e2167cbcf5006a669
Petter Ekedahl <petter.ekedahl@gmail.com>
2021-05-07 13:00:44 +0200

    Update actions.js
    Added the double/half-points action to the actions.js file.

--- actions/actions.js
+++ actions/actions.js
@@ -27,6 +27,34 @@ const actions = [
     target: 'stations',
     cooldown: 2 * 60,
   },
+  {
+    event: 'action:station:double-points',
+    name: 'Double Points Station',
+    desc: 'Doubles the amount of points handed out by a given Station. It only works for the next points.',
+    target: 'stations',
+    cooldown: 2 * 60,
+  },
+  {
+    event: 'action:station:half-points',
+    name: 'Half Points Station',
+    desc: 'Halves the amount of points handed out by a given Station. It only works for the next points.',
+    target: 'stations',
+    cooldown: 2 * 60,
+  },
+  {
+    event: 'action:stations:double-points',
+    name: 'Double All Points',
+    desc: 'Doubles the amount of points handed out by all Stations. It only works for the next points.',
+    target: 'stations',
+    cooldown: 2 * 60,
+  },
+  {
+    event: 'action:stations:half-points',
+    name: 'Half All Pooints',
+    desc: 'Halves the amount of points handed out by all Stations. It only works for the next points.',
+    target: 'stations',
+    cooldown: 2 * 60,
+  },
 ];
 
 module.exports = actions;
