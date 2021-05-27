61f85421312193dbbb72e2759b40311c9ff3d14e
Sebastian Bengtegård <sebastianbengtegard@gmail.com>
2021-05-27 16:48:17 +0200

    -

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-// This is where we `require` all of our routes
-const stats = require('./stats');
-
-// And then export them via `endpoint: router` pairs
-module.exports = {
-  '/stats': stats,
-};
